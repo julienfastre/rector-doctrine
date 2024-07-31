4c9ac7702ed1225fbe9f806f7da0038d788c4985
Julien Fastré <julien.fastre@champs-libres.coop>
2024-08-01 00:15:47 +0200

    Refactor the OrderByKeyToClassConstRector to use the new enum only in Criteria::orderBy method call.
    Relates to:
    
    - https://github.com/doctrine/collections/pull/389;
    - https://github.com/doctrine/orm/issues/11313#issuecomment-2214570323

--- rules/CodeQuality/Rector/Property/OrderByKeyToClassConstRector.php
+++ rules/CodeQuality/Rector/Property/OrderByKeyToClassConstRector.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Rector\Doctrine\CodeQuality\Rector\Property;
 
 use PhpParser\Node;
-use PhpParser\Node\Attribute;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Scalar\String_;
-use PhpParser\Node\Stmt\Property;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -21,31 +19,30 @@ final class OrderByKeyToClassConstRector extends AbstractRector
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
-            'Replace OrderBy Attribute ASC/DESC with class constant from Criteria',
+            'Replace OrderBy Attribute ASC/DESC with enum Ordering from Criteria',
             [
                 new CodeSample(
                     <<<'CODE_SAMPLE'
-use Doctrine\ORM\Mapping as ORM;
+                    <?php
 
-class ReplaceOrderByAscWithClassConstant
-{
-    #[ORM\OrderBy(['createdAt' => 'ASC'])]
-    protected \DateTimeInterface $messages;
-}
-?>
-CODE_SAMPLE
+                    use Doctrine\Common\Collections\Criteria;
 
+                    $criteria = new Criteria();
+                    $criteria->orderBy(['someProperty' => 'ASC', 'anotherProperty' => 'DESC']);
+
+                    ?>
+                    CODE_SAMPLE
                     ,
                     <<<'CODE_SAMPLE'
-use Doctrine\ORM\Mapping as ORM;
+                    <?php
 
-class ReplaceOrderByAscWithClassConstant
-{
-    #[ORM\OrderBy(['createdAt' => \Doctrine\Common\Collections\Criteria::ASC])]
-    protected \DateTimeInterface $messages;
-}
-?>
-CODE_SAMPLE
+                    use Doctrine\Common\Collections\Criteria;
+
+                    $criteria = new Criteria();
+                    $criteria->orderBy(['someProperty' => \Doctrine\Common\Collections\Order::Ascending, 'anotherProperty' => \Doctrine\Common\Collections\Order::Descending]);
+
+                    ?>
+                    CODE_SAMPLE
                 ),
             ]
         );
@@ -56,56 +53,87 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Property::class];
+        return [Node\Expr\MethodCall::class];
     }
 
     /**
-     * @param Property $node
+     * @param Node\Expr\MethodCall $node
      */
     public function refactor(Node $node): ?Node
     {
-        $nodeAttribute = null;
-        foreach ($node->attrGroups as $attrGroup) {
-            foreach ($attrGroup->attrs as $attr) {
-                if ($attr->name->toString() === 'Doctrine\ORM\Mapping\OrderBy') {
-                    $nodeAttribute = $attr;
-                    break 2;
+        // TODO this rule should not apply on PHP versions earlier that 8.1.0 and doctrine collection greater that 2.2.0
+        if (! $node->name instanceof Node\Identifier) {
+            return null;
+        }
+
+        if ((string) $node->name !== 'orderBy') {
+            return null;
+        }
+
+        if (count($node->args) < 1) {
+            return null;
+        }
+
+        // TODO find a way to ensure that the 'orderBy' method does apply on a \Doctrine\Common\Collections\Criteria instance
+
+        $arg = $node->args[0];
+        if (! $arg instanceof Node\Arg) {
+            return null;
+        }
+
+        if (! $arg->value instanceof Array_) {
+            return null;
+        }
+
+        $nodeHasChange = false;
+        // we parse the array here
+        foreach ($arg->value->items as $key => $item) {
+            if ($item === null) {
+                continue;
+            }
+
+            if ($item->value instanceof String_) {
+                $value = $item->value->value;
+                if ($value === 'ASC' || $value === 'asc') {
+                    $node->args[0]->value->items[$key]->value = $this->nodeFactory->createClassConstFetch(
+                        'Doctrine\Common\Collections\Order',
+                        'Ascending'
+                    );
+                    $nodeHasChange = true;
+                } elseif ($value === 'DESC' || $value === 'desc') {
+                    $node->args[0]->value->items[$key]->value = $this->nodeFactory->createClassConstFetch(
+                        'Doctrine\Common\Collections\Order',
+                        'Descending'
+                    );
+                    $nodeHasChange = true;
+                }
+            } elseif ($item->value instanceof Node\Expr\ClassConstFetch) {
+                if (! in_array('Criteria', $item->value->class->getParts())) {
+                    // TODO find a way to identify
+                    continue;
+                }
+
+                if ($item->value->name->toString() === 'ASC') {
+                    $node->args[0]->value->items[$key]->value = $this->nodeFactory->createClassConstFetch(
+                        'Doctrine\Common\Collections\Order',
+                        'Ascending'
+                    );
+                    $nodeHasChange = true;
+                } elseif ($item->value->name->toString() === 'DESC') {
+                    $node->args[0]->value->items[$key]->value = $this->nodeFactory->createClassConstFetch(
+                        'Doctrine\Common\Collections\Order',
+                        'Descending'
+                    );
+                    $nodeHasChange = true;
                 }
             }
+
         }
 
-        // If Attribute is not OrderBy, return null
-        if (! $nodeAttribute instanceof Attribute) {
-            return null;
+        if ($nodeHasChange) {
+            return $node;
         }
 
-        if (! isset($nodeAttribute->args[0])) {
-            return null;
-        }
-
-        if (! $nodeAttribute->args[0]->value instanceof Array_) {
-            return null;
-        }
-
-        if (! isset($nodeAttribute->args[0]->value->items[0])) {
-            return null;
-        }
-
-        if (! $nodeAttribute->args[0]->value->items[0]->value instanceof String_) {
-            return null;
-        }
-
-        // If Attribute value from key is not `ASC` or `DESC`, return null
-        if (! in_array($nodeAttribute->args[0]->value->items[0]->value->value, ['ASC', 'asc', 'DESC', 'desc'], true)) {
-            return null;
-        }
-
-        $upper = strtoupper($nodeAttribute->args[0]->value->items[0]->value->value);
-        $nodeAttribute->args[0]->value->items[0]->value = $this->nodeFactory->createClassConstFetch(
-            'Doctrine\Common\Collections\Criteria',
-            $upper
-        );
-
-        return $node;
+        return null;
     }
 }
